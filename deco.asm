4ea8e75a37c9db02b94e638b27451c8560e7d6e4
Oscar Méndez <48070861+Oscarmendez825@users.noreply.github.com>
2023-03-19 21:47:13 -0600

    Arreglo de bug al separar dos numeros y inicio de implementacion RSA

--- deco.asm
+++ deco.asm
@@ -1,8 +1,17 @@
 section .data
-    filename db '5.txt',0
-    buffer times 635918 db 0
-    num db 0 ;almacenará cada número encontrado
-    
+    filename db 'nums.txt',0
+    res db 'res.txt',0
+    ;num1 db 5 ;almacenará cada número encontrado
+    ;num2 db 5 ;almacenará cada número encontrado
+    mode db 'w', 0    ; modo de escritura
+
+section .bss
+    buffer resb 8
+    A resd 1
+    B resd 1
+    resultado resd 1
+    buffTemp resd 1
+    text resb 10         ; cadena de caracteres para almacenar el número convertido
 
 section .text
     global _start
@@ -22,20 +31,22 @@ _start:
     mov eax, 3  
     mov ebx, eax 
     mov ecx, buffer 
-    mov edx, 635918 
+    mov edx, 8
     int 80h ; llama al sistema
 
 ; print del archivo
-    mov eax, 4 
-    mov ebx, 1 
-    mov ecx, buffer 
-    int 80h 
+    ;mov eax, 4 
+    ;mov ebx, 1 
+    ;mov ecx, buffer 
+    ;int 80h 
 
 ; cerrar el archivo
     mov eax, 6 
     int 80h 
 
-    jmp _split
+    ;mov edi, 1 ;registro que controla cual split realizar
+
+    jmp _split1
 
 
 _error:
@@ -44,30 +55,102 @@ _error:
     int 80h 
 
 
-_split:
+_split1:
 	mov esi, buffer ;cargar la dirección del buffer en el registro ESI
 
 next_num:
 	xor eax, eax ;poner a cero el registro EAX
+    xor ebx, ebx
 
-next_char:
+createN1:
 	mov bl, [esi] ;cargar caracter en bl
 	cmp bl, 0 ;final del buffer cond parada
-	je end_split
+	je _RSA
 	cmp bl, ' ' ;si el carácter es un espacio fin del numero
-	je end_num
+	je end_num1
 	sub bl, '0' ;convertir el caracter a int
 	imul eax, 10 ;multiplicar el registro EAX por 10
 	add eax, ebx 
 	inc esi 
-	jmp next_char
+	jmp createN1
 
-end_num:
-	mov [num], al ;almacenar el número en memoria
+end_num1:
+	mov dword [A], eax ;almacenar el número en memoria
 	inc esi ;avanzar al siguiente carácter del buffer (el espacio en blanco)
-	jmp next_num
+	jmp _split2
 
-end_split:
+
+_split2:
+	xor eax, eax ;poner a cero el registro EAX
+    xor ebx, ebx
+next_char2:
+	mov bl, [esi] ;cargar caracter en bl
+	cmp bl, 0 ;final del buffer cond parada
+	je end_num2
+	cmp bl, ' ' ;si el carácter es un espacio fin del numero
+	je end_num2
+	sub bl, '0' ;convertir el caracter a int
+	imul eax, 10 ;multiplicar el registro EAX por 10
+	add eax, ebx 
+	inc esi 
+	jmp next_char2
+
+end_num2:
+	mov dword [B], eax ;almacenar el número en memoria
+	inc esi ;avanzar al siguiente carácter del buffer (el espacio en blanco)
+
+_RSA:
+    mov eax, 0 ;poner a cero el registro EAX
+    mov ebx, 0
+    mov eax, dword [A]
+    mov ebx, dword [B]
+    add eax,ebx
+
+_potencia:
+    mov ecx, 10 ;aqui va el valor de D
+    mov edx, 1 ;contador de la potencia
+    imul eax, eax
+    add edx, 1
+    cmp ecx, edx
+    jne _potencia
+    jmp _modulo
+
+_modulo:
+    xor edx, edx   ; Inicializar edx a cero
+    mov ebx, 3     ; Colocar el divisor en ebx
+    div ebx        ; Dividir eax por ebx
+    mov eax, edx   ; Mover el valor de edx a eax (el resto de la división)
+    mov dword [resultado], eax
+
+_convertChar:
+    ; mover el número a un registro
+    mov eax, dword [resultado]
+
+    ; convertir el número en una cadena de caracteres
+    mov ebx, 10        ; base decimal
+    xor ecx, ecx       ; contador de dígitos
+    xor edx, edx       ; limpiar edx para la división
+    .convert_loop:
+        div ebx          ; dividir eax por 10
+        add edx, '0'     ; convertir el resto a un carácter ASCII
+        mov [text + ecx], dl  ; guardar el carácter en la cadena de caracteres
+        inc ecx          ; incrementar el contador de dígitos
+        test eax, eax    ; comprobar si el cociente es cero
+        jnz .convert_loop ; si no es cero, seguir dividiendo
+
+
+
+
+
+
+_end_program:
+
+    ; print del archivo
+    mov eax, 4 
+    mov ebx, 1 
+    mov ecx, text 
+    int 80h 
+
 	mov eax, 1 ;preparar la llamada al sistema para salir
 	xor ebx, ebx ;código de salida 0
 	int 80h ;realizar la llamada al sistema
